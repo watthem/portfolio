3e6cd1e570c497122487a5647b687895bd574a9c
Matthew Hendricks <mscohendricks@gmail.com>
2020-01-21 15:48:33 -0800

    added ghost  plugin-config

--- gridsome-watthem/gridsome.config.js
+++ gridsome-watthem/gridsome.config.js
@@ -7,33 +7,43 @@
 module.exports = {
   siteName: 'Matthew Portfolio',
   plugins: [{
-    use: "@gridsome/source-filesystem",
-    options: {
-      // Path is the location of the markdown content we will be consuming in our collections.
-      path: "markdown/work/**/*.md",
-      // Typename is the GrapqhQL type and template name. A .vue file in src/tempaltes MUST MATCH (===) the typeName to have a template for it
-      typeName: "Work",
-      route: "/work/:title"
+      use: "@gridsome/source-filesystem",
+      options: {
+        // Path is the location of the markdown content we will be consuming in our collections.
+        path: "markdown/work/**/*.md",
+        // Typename is the GrapqhQL type and template name. A .vue file in src/tempaltes MUST MATCH (===) the typeName to have a template for it
+        typeName: "Work",
+        route: "/work/:title"
+      }
+    }, {
+      use: "@gridsome/source-filesystem",
+      options: {
+        // Path is the location of the markdown content we will be consuming in our collections.
+        path: "markdown/creative/**/*.md",
+        // Typename is the GrapqhQL type and template name. A .vue file in src/tempaltes MUST MATCH (===) the typeName to have a template for it
+        typeName: "Creative",
+        route: "/creative/:title"
+      }
+    }, {
+      use: "@gridsome/source-filesystem",
+      options: {
+        // Path is the location of the markdown content we will be consuming in our collections.
+        path: "markdown/demos/**/*.md",
+        // Typename is the GrapqhQL type and template name. A .vue file in src/tempaltes MUST MATCH (===) the typeName to have a template for it
+        typeName: "Demo",
+        route: "/demo/:title"
+      }
+    }, {
+      use: 'gridsome-plugin-tailwindcss',
+    },
+    {
+      use: '@gridsome/source-ghost',
+      options: {
+        typeName: 'Ghost',
+        baseUrl: 'http://localhost:2369',
+        contentKey: 'e48cd8bcb07a3dd1dd54457318',
+        version: 'v3' // default
+      }
     }
-  }, {
-    use: "@gridsome/source-filesystem",
-    options: {
-      // Path is the location of the markdown content we will be consuming in our collections.
-      path: "markdown/creative/**/*.md",
-      // Typename is the GrapqhQL type and template name. A .vue file in src/tempaltes MUST MATCH (===) the typeName to have a template for it
-      typeName: "Creative",
-      route: "/creative/:title"
-    }
-  }, {
-    use: "@gridsome/source-filesystem",
-    options: {
-      // Path is the location of the markdown content we will be consuming in our collections.
-      path: "markdown/demos/**/*.md",
-      // Typename is the GrapqhQL type and template name. A .vue file in src/tempaltes MUST MATCH (===) the typeName to have a template for it
-      typeName: "Demo",
-      route: "/demo/:title"
-    }
-  }, {
-    use: 'gridsome-plugin-tailwindcss',
-  }]
+  ]
 }
