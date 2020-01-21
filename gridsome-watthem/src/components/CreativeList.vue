f62f44fd10e0b926abcdaedd0b015853e4d6ae69
Matthew Hendricks <mscohendricks@gmail.com>
2020-01-21 10:01:50 -0800

    added clientonly tag to carousel containers

--- gridsome-watthem/src/components/CreativeList.vue
+++ gridsome-watthem/src/components/CreativeList.vue
@@ -1,29 +1,31 @@
 <template>
   <div class>
-    <simple-carousel-container loop>
-      <simple-carousel-item v-for="(edge) in $static.featuredCreative.edges" :key="edge.node.id">
-        <div class="py-8 px-4 m-auto rounded shadow-lg bg-white">
-          <g-link :to="edge.node.path">
-            <h3 class="text-2xl mb-4 font-heading">{{ edge.node.title }}</h3>
-          </g-link>
-          <div class="px-6 py-4">
-            <span class="text-gray-900 leading-none">I published it on</span>
-            <span
-              class="inline-block bg-gray-300 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 m-1"
-            >{{ edge.node.Published_On }}.</span>
+    <ClientOnly>
+      <simple-carousel-container loop>
+        <simple-carousel-item v-for="(edge) in $static.featuredCreative.edges" :key="edge.node.id">
+          <div class="py-8 px-4 m-auto rounded shadow-lg bg-white">
+            <g-link :to="edge.node.path">
+              <h3 class="text-2xl mb-4 font-heading">{{ edge.node.title }}</h3>
+            </g-link>
+            <div class="px-6 py-4">
+              <span class="text-gray-900 leading-none">I published it on</span>
+              <span
+                class="inline-block bg-gray-300 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 m-1"
+              >{{ edge.node.Published_On }}.</span>
+            </div>
+            <g-link :to="edge.node.path">
+              <p class="text-gray-500 leading-relaxed text-md">{{ edge.node.Stub}}</p>
+            </g-link>
+            <div class="content-end text-right">
+              <a
+                class="px-3 text-blue-700 underline hover:text-white hover:bg-blue-700"
+                :href="edge.node.path"
+              >read more</a>
+            </div>
           </div>
-          <g-link :to="edge.node.path">
-            <p class="text-gray-500 leading-relaxed text-md">{{ edge.node.Stub}}</p>
-          </g-link>
-          <div class="content-end text-right">
-            <a
-              class="px-3 text-blue-700 underline hover:text-white hover:bg-blue-700"
-              :href="edge.node.path"
-            >read more</a>
-          </div>
-        </div>
-      </simple-carousel-item>
-    </simple-carousel-container>
+        </simple-carousel-item>
+      </simple-carousel-container>
+    </ClientOnly>
   </div>
 </template>
 
